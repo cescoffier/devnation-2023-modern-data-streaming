344034716945505c129457f0890c7b6240d76f8f
Clement Escoffier <clement@apache.org>
2023-06-19 14:47:17 +0200

    Automate picture analyzer deployment

--- alert-manager/src/main/java/me/escoffier/alert/PredictionService.java
+++ alert-manager/src/main/java/me/escoffier/alert/PredictionService.java
@@ -6,7 +6,7 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
-@RegisterRestClient(baseUri = "https://snapshot-analyzer-devnation.apps.cluster-9lgs8.9lgs8.sandbox272.opentlc.com/")
+@RegisterRestClient(baseUri = "https://picture-analyzer-devnation.apps.cluster-9lgs8.9lgs8.sandbox272.opentlc.com/")
 public interface PredictionService {
 
     @Path("/predictions")
