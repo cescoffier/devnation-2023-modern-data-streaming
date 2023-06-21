866518de3b0fd83d5744a3d47c48f0be776ce7ba
Clement Escoffier <clement@apache.org>
2023-06-21 15:24:42 +0200

    Update the images to use the devnation organization on quay.io

--- picture-analyzer/build-and-push.sh
+++ picture-analyzer/build-and-push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-NAME=quay.io/cescoffi/devnation-2023-picture-analyzer
-docker buildx build --platform=amd64 -t ${NAME} .
+NAME=quay.io/devnation/devnation-2023-picture-analyzer
+docker buildx build --platform=linux/amd64 -t ${NAME} .
 docker push ${NAME}
